c00453d737e27a92cb479b7a96d3c16a89a79662
fran-alex-deanda <100243088+fran-alex-deanda@users.noreply.github.com>
2022-03-18 19:41:16 -0500

    Initial Commit.

---
+++ simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -0,0 +1,46 @@
+package q1.extract_method.refactored;
+
+import java.util.List;
+
+//import p5.extract_method_typeparameterize.refactored.Graph;
+
+public class A {
+   Node m1(List<Node> nodes, String p) {
+	   extractedMethod(nodes,p);
+      // other implementation
+      return null;
+   }
+
+   Edge m2(List<Edge> edgeList, String p) {
+	   extractedMethod(edgeList,p);
+      // other implementation
+      return null;
+   }
+
+   /**
+    * Extract Method Refactoring by Type Parameterizing
+    */
+   <T extends Graph> void extractedMethod(List<T> objs, String p) {
+      for (T obj : objs) {
+         if (obj.contains(p))
+            System.out.println(obj);
+      }
+   }
+}
+
+/**
+ * Extract Superclass Refactoring
+ */
+class Graph {
+   String name;
+
+   public boolean contains(String p) {
+      return name.contains(p);
+   }
+}
+
+class Node extends Graph{
+}
+
+class Edge extends Graph{
+}
